0c53d1c123b40a184e3198f0fe9c9d408599247e
Marcelo <50717722+marcelowsky@users.noreply.github.com>
2022-06-24 21:53:41 -0300

    Teoria, exercícios práticos e desafio

---
+++ Modulo_II_Fundamentos_web_com_HTML_e_CSS/Introdução a criação de web sites com HTML5 e CSS/Página.php
@@ -0,0 +1,73 @@
+<html>
+
+
+<head>
+
+<img src="Imagens\marcelo.jpg" alt="Imagem de Marcelo"/ class="image-3">
+
+<title> Nosso Universo !</title>
+
+<link rel="stylesheet" href="style_css.php"/>
+			
+</head>
+	
+	
+<body>
+
+<header id="cabecalho">
+
+<h1 id="title" >O que é o Universo? </h1>
+
+<h3> <a href="mailto:msabargi@gmail.com" target="blank"> Por Marcelo Arruda </a> </h3>
+
+<h5> <a href= "https://pt.wikipedia.org/wiki/Universo" target= "blank"> Origem: Wikipédia, a enciclopédia livre </a> </h5>
+
+
+
+</header id="cabecalho">
+
+<header id="article">
+
+<img src= "Imagens\universo_1.png" alt="Imagem do Universo"/ class="image-1"> 
+
+
+<p> O <b>Universo</b> é tudo o que existe fisicamente, a soma do espaço e do tempo e as mais variadas formas de matéria, como planetas, estrelas, galáxias e os componentes do espaço intergaláctico.O termo Universo pode ser usado em sentidos contextuais ligeiramente diferentes, denotando conceitos como o cosmo, o mundo ou a natureza. O universo observável tem de raio cerca de 46 bilhões de anos-luz. A observação científica do Universo levou a inferências de suas fases anteriores. Estas observações sugerem que o Universo é governado pelas mesmas leis físicas e constantes durante a maior parte de sua extensão e história. A teoria do Big Bang é o modelo cosmológico prevalente que descreve como o Universo evoluiu desde os primeiros 10-44 segundos (Tempo de Planck) até hoje. Observações de supernovas têm mostrado que o Universo está se expandindo a uma velocidade acelerada.
+<p>Os valores anteriores para o número de galáxias no Universo giravam em, aproximadamente, cem bilhões de galáxias. Mas em outubro de 2016 dados reunidos em duas décadas de imagens colhidas pelo Hubble mostraram que o número de galáxias gira em torno de 20 vezes mais, saltando para 2 trilhões de galáxias, aproximadamente. Os espaços vazios do Universo podem estar repletos de matéria escura, de natureza ainda desconhecida. De acordo com o modelo científico vigente, conhecido como Big Bang, o Universo surgiu de um único ponto ou singularidade onde toda a matéria e energia do universo observável encontrava-se concentrada numa fase densa e extremamente quente chamada Era de Planck.</p>
+A partir dessa era, o Universo vem-se expandindo, possivelmente em curtos períodos (menos que 10−32 segundos) de inflação cósmica. Diversas medições experimentais independentes apoiam teoricamente tal expansão e a teoria do Big Bang. Esta expansão tem-se acelerado por ação da energia escura, uma força contrária à gravidade que está agindo mais que esta devido ao fato das dimensões do Universo serem grandes o bastante para dissipar a força gravitacional. Porém, graças ao escasso conhecimento a respeito da energia escura, é ainda pequeno o entendimento do fenômeno e sua influência no destino do Universo.</p>
+Há alguns anos, a sonda WMAP coletou dados que levaram à determinação da Idade do Universo em 13,73 (± 0,12) bilhões de anos. Entretanto, com base em dados coletados pelo satélite Planck, as interpretações de observações astronômicas indicam que a idade do Universo seria de 13,799 ± 0,021 bilhões de anos, enquanto o diâmetro do universo observável seria de 91 bilhões de anos-luz ou 8,80×1026 metros. De acordo com a teoria da relatividade geral, o espaço pode expandir-se a uma velocidade superior à da luz, embora possamos ver somente uma pequena fração da matéria visível do Universo devido à limitação imposta pela velocidade da luz. É incerto se a dimensão do espaço é finita ou infinita. Trezentos mil anos depois do Big Bang, teriam surgido átomos de matéria. As formas de vida teriam aparecido 11,2 bilhões de anos depois.
+ 
+<h2 class="post_title"> Etimologia </h2>
+
+A palavra Universo deriva do latim universum. A palavra latina foi usada por Cícero e posteriormente por outros autores com o mesmo sentido que é empregada atualmente. A palavra latina é derivada da contração poética unvorsum — utilizada primeiramente por Lucrécio no Livro IV (linha 262) de seu De rerum natura (Sobre a Natureza das coisas) — que conecta un, uni (a forma combinada de unus, ou "one") com vorsum, versum (um substantivo derivado do particípio passivo perfeito de vertere, que significa "algo rodado, rolado ou mudado").
+Uma interpretação alternativa de unvorsum é "tudo girando como um" ou "tudo girando através de um". Nesse sentido, pode ser considerada a tradução de uma palavra para Universo no grego antigo, περιφορα, "algo transportado em um círculo", originalmente utilizada para descrever o percurso de uma refeição, a comida sendo carregada em torno de um círculo de mesas.
+
+<h2 class="post_title"> Cronologia </h2>
+
+<p>O modelo prevalecente para a evolução do Universo é a teoria do Big Bang. O modelo do Big Bang afirma que o estado mais antigo do Universo era extremamente quente e denso e que posteriormente se expandiu. O modelo baseia-se na relatividade geral e na simplificação de suposições como a homogeneidade e a isotropia do espaço. Uma versão do modelo com uma constante cosmológica (Lambda) e matéria escura fria, conhecida como modelo Lambda-CDM, é o modelo mais simples que fornece um relato razoavelmente bom de várias observações sobre o Universo. O modelo do Big Bang é responsável por observações como a correlação da distância e o desvio para o vermelho das galáxias, a razão entre o número de átomos de hidrogênio e de hélio e a radiação cósmica de fundo.
+O estado quente e denso inicial é chamado de era de Planck, um breve período que se estende do tempo zero a uma unidade de tempo de Planck de aproximadamente 10-43 segundos.</p> <p>Durante a época de Planck, todos os tipos de matéria e todos os tipos de energia estavam concentrados em um estado denso, onde acredita-se que a gravitação tenha sido tão forte quanto as outras forças fundamentais, sendo que todas as forças podem ter sido unificadas. Desde a época de Planck, o Universo vem se expandindo para sua forma atual, possivelmente com um período muito breve de inflação cósmica que fez com que o Universo atingisse um tamanho muito maior em menos de 10-32 segundos.</p>
+Depois da época de Planck e da inflação, vieram as épocas de quark, hadron e lépton. Juntas, essas épocas abrangiam menos de dez segundos de tempo após o Big Bang. A abundância observada dos elementos pode ser explicada pela combinação da expansão global do espaço com a física nuclear e atômica. À medida que o Universo se expande, a densidade de energia da radiação eletromagnética diminui mais rapidamente do que a da matéria, porque a energia de um fóton diminui com seu comprimento de onda.
+À medida que o Universo se expandia e se esfriava, partículas elementares associavam-se de forma estável a combinações cada vez maiores. Assim, na primeira parte da era dominada pela matéria, formaram-se prótons e nêutrons estáveis, que então formaram núcleos atômicos através de reações nucleares. Este processo, conhecido como nucleossíntese do Big Bang, levou à abundância presente de núcleos mais leves, particularmente hidrogênio, deutério e hélio. A nucleossíntese do Big Bang terminou cerca de vinte minutos após o Big Bang, quando o Universo tinha esfriado o suficiente para que a fusão nuclear não pudesse mais ocorrer. Nesta fase, a matéria no Universo era principalmente um plasma quente e denso de elétrons carregados negativamente, neutrinos neutros e núcleos positivos. Esta era, chamada época fotônica, durou cerca de 380 mil anos.
+Eventualmente, em um momento conhecido como recombinação, elétrons e núcleos formaram átomos estáveis, que são transparentes para a maioria dos comprimentos de onda de radiação. Com os fótons dissociados da matéria, o Universo entrou na era dominada pela matéria. A luz desta era podia viajar livremente e pode ainda ser vista no Universo como radiação cósmica de fundo. Depois de cerca de cem milhões de anos, as primeiras estrelas se formaram; estas eram provavelmente muito maciças, luminosas e responsáveis pela reionização do Universo. Não tendo elementos mais pesados que o lítio, essas estrelas também produziram os primeiros elementos pesados através da nucleossíntese estelar. O Universo também contém uma energia misteriosa chamada energia escura, cuja densidade não muda ao longo do tempo. Após cerca de 9,8 bilhões de anos, o Universo se expandiu suficientemente para que a densidade da matéria fosse menor que a densidade da energia escura, marcando o início da atual era dominada pela energia escura. Nesta época, a expansão do Universo está se acelerando devido à energia escura.
+
+<img src= "Imagens\universo_2.png" alt="Imagem do Universo" class="image-2">
+
+</header id="article">
+
+
+<footer> 
+
+<ul class="contact-list"/>
+<li>
+<a href="mailto:msabargi@gmai.com"target="blank"> msabargi@gmail.com </a> <br />
+
+</li>
+<li>
+<a href="https://www.linkedin.com/in/ms-arruda" target="blank"> LinkedIn</a>
+</li>
+
+</footer>
+
+</body>
+		
+</html>
+		
---
+++ Modulo_II_Fundamentos_web_com_HTML_e_CSS/Introdução a criação de web sites com HTML5 e CSS/README.md
@@ -0,0 +1,2 @@
+# Desenvolvimento-Web
+Curso de Desenvolvimento em HTML5 e CSS da DIO
